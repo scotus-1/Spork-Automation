024cd5d947a5bb2fb6a02555371860df530bd926
Incompleteusern <58920010+Incompleteusern@users.noreply.github.com>
2020-11-22 09:58:37 -0700

    check if login succeeds, fixed an error
    staleness = WebDriverWait(self.driver, 3).until(ec.staleness_of(passwordField))
        - ec.staleness_of detects if the elment, namely the password, is still connected to the DOM, basically the page
    
    success = client.enter_credentials()
       - Only clicks join button if the credentials have succeeded

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.common import exceptions
-
-import json
-
-
 def parse_json(json_file_path):
     with open(json_file_path, 'r') as json_file:
         dictionary = json.load(json_file)
         return dictionary
-
+    
 class SporkInstance:
     def __init__(self, driver_path, is_headless, json_creds_path= "creds.json"):
         if is_headless:
@@ -34,19 +24,29 @@ class SporkInstance:
             passwordField.clear()
             passwordField.send_keys(self.credentials.get("password"))
             passwordField.send_keys(Keys.ENTER)
+            
+            #returns true if the login is no longer attached to the DOM, i.e. no longer there
+            #else makes an error
+            staleness = WebDriverWait(self.driver, 3).until(ec.staleness_of(passwordField))
+
+            #whether to continue, as it would produce an error if it tried to use webdriver and it quit
+            return True
         except (exceptions.NoSuchElementException, exceptions.TimeoutException):
+            print("Unable to enter credentials")
             self.driver.quit()
+            return False
 
     def click_join_button(self):
         try:
             joinButtons = WebDriverWait(self.driver, 10).until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, 'button.ui.green.compact.button')))
-            for i in joinButtons:
-                i.click()
+            for button in joinButtons:
+                button.click()
         except (exceptions.NoSuchElementException, exceptions.TimeoutException):
-            print("no button")
+            print("No button to press")
             self.driver.quit()
 
 if __name__ == "__main__":
-    client = SporkInstance("chromedriver.exe", False, "creds.json")
-    client.enter_credentials()
-    client.click_join_button()
+    client = SporkInstance("path", False, "creds.json")
+    success = client.enter_credentials()
+    if (success):
+        client.click_join_button()
